47ab76222958b3a9d2f6f706d449b3236e294297
brandonholl <113564252+brandonholl@users.noreply.github.com>
2022-09-28 11:04:39 -0400

    Create hw 2.py

---
+++ hw 2.py
@@ -0,0 +1,31 @@
+#3
+1 is equal to true as an integer
+
+#4
+0 is equal to false as an integer
+
+#6
+a. True
+b. True
+c. False
+d. True
+e. False
+f. True
+g. True
+h. False
+i. False
+j. True
+k. True
+l. False 
+
+#9
+a. x < y or x > y
+b. x < y or x == y
+c. x > y or x == y 
+d. x < y
+e. x > y 
+f. x == y
+g. x == y 
+h. x != y and x > 2
+i. x != y or x < 2
+j. x == y
